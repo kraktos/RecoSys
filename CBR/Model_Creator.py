25cf9714aa3aa96be1891ab09b2a3668e7a03385
arnabdutta <arnab.dutta@number26.de>
2016-10-04 17:50:04 +0200

    CBR, base 1

---
+++ CBR/Model_Creator.py
@@ -0,0 +1,7 @@
+"""
+The core idea behind a CBR system is to train a model on each of the users personal
+preferences. Given a set of features as predictors and a target variable which is the rating,
+it is possible to learn a parameter vector theta. Once theta is learned, we can predict the
+likely rating for any given rating.
+
+"""
