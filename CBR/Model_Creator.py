04f949bacab76f0cc048224b1d5402febfc7c703
arnabdutta <arnab.dutta@number26.de>
2016-10-04 17:51:35 +0200

    CBR base

--- CBR/Model_Creator.py
+++ CBR/Model_Creator.py
@@ -3,5 +3,4 @@ The core idea behind a CBR system is to train a model on each of the users perso
 preferences. Given a set of features as predictors and a target variable which is the rating,
 it is possible to learn a parameter vector theta. Once theta is learned, we can predict the
 likely rating for any given rating.
-
 """
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RecoSys
 A plain old Recommender System
+
+The idea is to do the whole thing from scratch, and avoiding use of any tools like MLLib or Graphlab
